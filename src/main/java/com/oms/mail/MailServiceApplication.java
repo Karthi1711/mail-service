9ecedb8c7a1069bb93b83ca23078d257535d3eaa
RAJASEKAR VADAMALAIMUTHU <Rajasekar.Vadamalaimuthu@merrillcorp.com>
2017-10-06 17:28:01 +0530

    OMS-000-Enabled-eureka-client

--- src/main/java/com/oms/mail/MailServiceApplication.java
+++ src/main/java/com/oms/mail/MailServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableOMSCommonWeb
+@EnableEurekaClient
 public class MailServiceApplication {
 
 	public static void main(String[] args) {
